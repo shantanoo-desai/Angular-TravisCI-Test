eefa708ddd3e6d26452544c14e976584837097a1
Shantanoo <shantanoo.desai@gmail.com>
2018-07-04 17:24:08 +0200

    add karma conf file
    Signed-off-by: Shantanoo <shantanoo.desai@gmail.com>

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'), /* add PhantomJS launcher here */
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma')
+    ],
+    /*...*/
+    browsers: ['Chrome', 'PhantomJS'], /* add PhatomJS here */
+    singleRun: false
+  });
+};
