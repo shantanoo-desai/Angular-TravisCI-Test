4696414a988a987ba589dd54f3dbc87e11b8ddd8
Shantanoo <shantanoo.desai@gmail.com>
2018-07-04 17:21:44 +0200

    Added Initial test case
    Signed-off-by: Shantanoo <shantanoo.desai@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,20 @@ describe('AppComponent', () => {
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('TESTANG_1');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angulartesting!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TESTANG_1');
   }));
+
+  it('increase number when button pressed', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.points).toBe(1);
+    fixture.debugElement.nativeElement.querySelector('button').click();
+    expect(fixture.componentInstance.points).toBe(2);
+  }))
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'TESTANG_1';
+  points = 1;
+
+  plus1() {
+    this.points++;
+  }
 }
